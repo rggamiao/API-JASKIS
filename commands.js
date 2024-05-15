39830be1e990fcb2506b59b2c4ad7813a71cddf5
rggamiao <rejicamiling@gmail.com>
2024-05-15 14:50:09 -0700

    First commit

--- commands.js
+++ commands.js
@@ -3,14 +3,23 @@
 
 // GETTING STARTED
 // 1. Create a database called jaskis
+use jaskis
 
 
 // 2. Create a collection called bounties
-
+debugger.createCollection('bounties')
 
 // ADD THE ANIMAL BOUNTIES
 // 1. Insert the given "Thanoceros" bounty object
-
+db.bounties.insertOne({
+    name: "Thanoceros",
+    species: "Rhinoceros",
+    location: "Grasslands",
+    wantedFor: "Eating too much grass",
+    client: "Songbird",
+    reward: 10000,
+    captured: false
+  })
 
 // 2. Query for all bounties in the bounties collection
 
