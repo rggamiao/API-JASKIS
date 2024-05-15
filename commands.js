3a24685962f36e8112b1cfe42a72735ccb4214ce
rggamiao <rejicamiling@gmail.com>
2024-05-15 14:53:51 -0700

    second commit

--- commands.js
+++ commands.js
@@ -22,13 +22,69 @@ db.bounties.insertOne({
   })
 
 // 2. Query for all bounties in the bounties collection
-
+db.bounties.find({}).pretty();
 
 // 3. Insert many bounties at once using the given objects
-
+db.bounties.insertMany([
+    {
+        "name": "Lokinkajou",
+        "species": "Kinkajou",
+        "location": "Tropical rainforest",
+        "wantedFor": "Partying too late at night",
+        "client": "White tiger",
+        "reward": 1000,
+        "captured": false
+      },
+      {
+        "name": "Nebullama",
+        "species": "Llama",
+        "location": "Grasslands",
+        "wantedFor": "Drinking all the water from the ocean",
+        "client": "Songbird",
+        "reward": 2500,
+        "captured": false
+      },
+      {
+        "name": "Polarwind",
+        "species": "Polar Bear",
+        "location": "Arctic",
+        "wantedFor": "Not hibernating",
+        "client": "Sabertooth",
+        "reward": 4000,
+        "captured": false
+      },
+      {
+        "name": "Wrecking Crows",
+        "species": "Crow",
+        "location": "Grasslands",
+        "wantedFor": "Cawing too loudly",
+        "client": "Red wolf",
+        "reward": 40000,
+        "captured": false
+      },
+      {
+        "name": "Grandhog",
+        "species": "Groundhog",
+        "location": "Woodlands",
+        "wantedFor": "Not coming out of the hole on time and prolonging winter",
+        "client": "Songbird",
+        "reward": 50000,
+        "captured": false
+      },
+      {
+        "name": "Grim Panda",
+        "species": "Giant Panda",
+        "location": "Temperate forest",
+        "wantedFor": "Eating all the bamboo",
+        "client": "Red wolf",
+        "reward": 5000,
+        "captured": false
+      }
+])
 // MANAGE THE DATABASE
 // Queries
 // 1. Query for all bounties in the Grasslands
+db.bounties.find([ location: "Grasslands" })
 
 // 2. Query for all bounties with a reward worth 10000 or more
 
