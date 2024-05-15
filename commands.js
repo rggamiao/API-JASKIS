b5e85d5f37e88601aa6f73c9f37377b4a632a961
rggamiao <rejicamiling@gmail.com>
2024-05-15 15:41:19 -0700

    final revision

--- commands.js
+++ commands.js
@@ -87,16 +87,17 @@ db.bounties.insertMany([
 db.bounties.find([ location: "Grasslands" })
 
 // 2. Query for all bounties with a reward worth 10000 or more
-
+db.bounties.find({ reward: {$gte: 10000} })
 // 3. Query for all bounties, but exclude the client attribute from being shown
-
+db.bounties.find( {}, { client: 0 } );
 // 4. Query for a Groundhog in the Woodlands
-
+db.bounties.find( { species: "Groundhog", location: "Woodlands" });
 // Update and Delete
 // 1. Update the reward for Polarwind to 10000
-
+db.bounties.updateOne( { name: "Polarwind", species: "Polar Bear" }, { $set: { reward: 10000 } });
 // 2. Remove Lokinkajou
-
+db.bounties.deleteOne({name: "Lokinkajou"})
 // 3. Delete all bounties sent by Songbird
-
+db.bounties.deleteMany({ client: "Songbird" });
 // 4. Update all captured statuses to true
+db.bounties.updateMany( {}, { $set: { captured: true } });
